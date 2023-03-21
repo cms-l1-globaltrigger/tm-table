955a7204828a028949576430e70a283a467dfade
Bernhard Arnold <bernhard.arnold@cern.ch>
2023-03-21 16:05:34 +0100

    utm 0.11.2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ import os
 from setuptools import setup, Extension
 import setuptools.command.build_py
 
-UTM_VERSION = '0.11.1'
+UTM_VERSION = '0.11.2'
 PACKAGE_NAME = 'tmTable'
 PACKAGE_DIR = os.path.realpath(os.path.join(os.path.dirname(__file__), PACKAGE_NAME))
 
